4aee0326d2936135afabf2f162f24f896b1544ca
Prath <a@b.com>
2016-08-30 14:16:59 -0400

    MyFirstPush

---
+++ MavenDemo/src/main/java/com/test/Junittest.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnit4ClassRunner.class)
+public class Junittest {
+	
+	@Test
+	
+	public void sayHello(){
+		System.out.println("Hello ....");
+		
+	}
+
+}
